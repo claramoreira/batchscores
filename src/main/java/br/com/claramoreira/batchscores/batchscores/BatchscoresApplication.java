0d20d936107ab6217fba60737ce7c9f910c50762
Clara <clara_ljm@hotmail.com>
2023-03-13 19:02:59 -0300

    gitignore update

---
+++ src/main/java/br/com/claramoreira/batchscores/batchscores/BatchscoresApplication.java
@@ -0,0 +1,13 @@
+package br.com.claramoreira.batchscores.batchscores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchscoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchscoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/claramoreira/batchscores/batchscores/BatchscoresApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.claramoreira.batchscores.batchscores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchscoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
